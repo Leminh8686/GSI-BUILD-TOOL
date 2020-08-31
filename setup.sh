277136ba5e1b6568eea90564bfe1bff8f050b987
Leminh8686 <19506021+Leminh8686@users.noreply.github.com>
2020-08-31 21:37:23 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone --recurse-submodules https://github.com/together08/ErfanGSIs-Mod.git --depth=1 erfangsi
+git clone --recurse-submodules https://github.com/Leminh8686/ErfanGSIs.git --depth=1 erfangsi
 bash ./erfangsi/setup.sh
